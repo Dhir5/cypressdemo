21533a002683693b30590769289e69011aacd074
Dhiraj Kumar Sah <dhirajsah1435@gmail.com>
2025-09-06 17:14:28 +0530

    add globally timeout

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
+    
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
       return config;
